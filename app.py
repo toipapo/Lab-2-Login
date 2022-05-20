9c810cd68e61b2469ee904186e005173a0e9090f
toipapo <michaeljoy.rodriguez@g.msuiit.edu.ph>
2022-05-20 15:10:29 +0800

    Lab 3 OTP Login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, render_template, session, url_for
+import smtplib
+from flask import Flask, redirect, render_template, session, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL, MySQLdb
 from flask_wtf import FlaskForm
@@ -8,6 +9,8 @@ import random, string
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "thisisasecret"
+otp = ''
+hasRequestedOTP = False
 Bootstrap(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -22,9 +25,8 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=50)])
 
-class ChangePasswordForm(FlaskForm):
-    oldPassword = PasswordField('Old Password', validators=[InputRequired(), Length(min=8, max=50)])
-    newPassword = PasswordField('New Password', validators=[InputRequired(), Length(min=8, max=50)])
+class OTPForm(FlaskForm):
+    otp = StringField('OTP', validators=[InputRequired(), Length(min=6, max=6)])
 
 @app.route('/')
 def index():
@@ -33,10 +35,21 @@ def index():
 
 @app.route('/profilepage')
 def profilepage():
+    global otp
+    global hasRequestedOTP
     return render_template('profilepage.html', username = session['users_username'])
+
+@app.route('/otppage')
+def otppage():
+    global otp
+    global hasRequestedOTP
+    form = OTPForm()
+    return render_template('otppage.html', form = form, username = session['users_username'])
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    global otp
+    global hasRequestedOTP
     form = LoginForm()
     if form.validate_on_submit():
         message = "Invalid username and password"
@@ -45,59 +58,59 @@ def login():
         key = "secretkey"
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(''' SELECT users_emp_idnum, users_username FROM users WHERE users_username = %s AND AES_DECRYPT(users_password, %s) = %s''', (username, key, password))
+        cursor.execute(''' SELECT users_emp_idnum, users_username FROM users WHERE users_username = %s 
+        AND AES_DECRYPT(users_password, %s) = %s''', (username, key, password))
         user = cursor.fetchall()
 
         if len(user) == 1:
             session['loggedin'] = True
             session['users_emp_idnum'] = user[0]['users_emp_idnum']
             session['users_username'] = user[0]['users_username']
-            return redirect(url_for('profilepage', form = form, username = session['users_username']))
+            return redirect(url_for('otp_request', form = form, username = session['users_username']))
 
         else:
             return render_template('loginpage.html', form = form, message = message)
 
+def otp_generator():
+    number_string = ''
+    for i in range (0,6):
+        number_string += str(random.randint(0,9))
+    return number_string
+
+@app.route('/otp_request', methods=['POST', 'GET'])
+def otp_request():
+    global otp
+    global hasRequestedOTP
+
+    if hasRequestedOTP == False:
+        otp = otp_generator()
+        hasRequestedOTP = True
+
+        email_request = smtplib.SMTP("smtp.gmail.com", 587)
+        email_request.starttls
+        email_request.login("michaeljoy.rodriguez@g.msuiit.edu.ph", "vuyzzmqldssgkiam")
+        email = "michaeljoy.rodriguez@g.msuiit.edu.ph"
+        email_request.sendmail('&&&&&&&&&&&',email, str(otp))
+
+    if request.method == 'POST':
+        if otp == request.form['otp']:
+            return redirect(url_for('profilepage'))
+        else:
+            return "<p>" + "Incorrect OTP was entered" + "</p>"
+    else:
+        return render_template('ottpage.html', context = {"status": otp})
+        
+
 @app.route('/logout', methods = ['GET', 'POST'])
 def logout():
+    global otp
+    global hasRequestedOTP
     session.pop('loggedin', None)
     session.pop('users_emp_idnum', None)
     session.pop('users_username', None)
+    hasRequestedOTP = False
+    otp = ''
     return redirect(url_for("index"))
-
-@app.route('/changepasspage')
-def changepasspage():
-    form = ChangePasswordForm()
-    return render_template('changepass.html', form = form)
-
-@app.route('/changepassword', methods = ['GET', 'POST'])
-def changepassword():
-    form = ChangePasswordForm()
-    if form.validate_on_submit():
-        username = session['users_username']
-        oldPassword = form.oldPassword.data
-        newPassword = form.newPassword.data
-        key = "secretkey"
-
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(''' UPDATE users SET users_password = AES_ENCRYPT(%s, %s) WHERE users_username = %s AND AES_DECRYPT(users_password, %s) = %s ''', (newPassword, key, username, key, oldPassword))
-        mysql.connection.commit()
-        cursor.close()
-        return render_template('profilepage.html', form = form, username = session['users_username'])
-
-@app.route('/resetpassword', methods = ['GET', 'POST'])
-def resetpassword():
-    letters = string.ascii_letters
-    digits = string.digits
-    username = session['users_username']
-    key = "secretkey"
-    newPassword = "".join(random.choice(letters + digits) for i in range(10))
-
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute(''' UPDATE users SET users_password = AES_ENCRYPT(%s, %s) WHERE users_username = %s''', (newPassword, key, username))
-    mysql.connection.commit()
-    cursor.close()
-    return render_template("profilepage.html", username = session['users_username'], password = newPassword)
-
 
 if __name__ == "__main__":
     app.run(debug = True)
